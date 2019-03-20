8fb313fb7ade5b40f17177775c2be76ab38398d4
rooftopwang <wolfgangwang@hotmail.ca>
2019-03-20 00:32:09 -0400

    custom.js hide google map function

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,43 +6,43 @@ $(window).load(function(){
 });
 
 
- /* Google Map
------------------------------------------------*/
-var map = '';
-var center;
+//  /* Google Map
+// -----------------------------------------------*/
+// var map = '';
+// var center;
 
-function initialize() {
-    var mapOptions = {
-      zoom: 16,
-      center: new google.maps.LatLng(13.758468, 100.567481),
-      scrollwheel: false
-    };
+// function initialize() {
+//     var mapOptions = {
+//       zoom: 16,
+//       center: new google.maps.LatLng(13.758468, 100.567481),
+//       scrollwheel: false
+//     };
   
-    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+//     map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
 
-    google.maps.event.addDomListener(map, 'idle', function() {
-        calculateCenter();
-    });
+//     google.maps.event.addDomListener(map, 'idle', function() {
+//         calculateCenter();
+//     });
   
-    google.maps.event.addDomListener(window, 'resize', function() {
-        map.setCenter(center);
-    });
-}
+//     google.maps.event.addDomListener(window, 'resize', function() {
+//         map.setCenter(center);
+//     });
+// }
 
-function calculateCenter() {
-  center = map.getCenter();
-}
+// function calculateCenter() {
+//   center = map.getCenter();
+// }
 
-function loadGoogleMap(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-    document.body.appendChild(script);
-}
+// function loadGoogleMap(){
+//     var script = document.createElement('script');
+//     script.type = 'text/javascript';
+//     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+//     document.body.appendChild(script);
+// }
 
-$(function(){
-  loadGoogleMap();
-});
+// $(function(){
+//   loadGoogleMap();
+// });
 
 
 /* Istope Portfolio
